d3f8d323e8ddd951d883f7c5d69d0c4c1bbfc9c2
JonatanStreith <jonatan@streith.se>
2018-06-11 15:59:37 +0200

    g

--- JS_Identity/JS_Identity/Controllers/HomeController.cs
+++ JS_Identity/JS_Identity/Controllers/HomeController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
+using JS_Identity.Models;
+using Microsoft.AspNet.Identity;
 
 namespace JS_Identity.Controllers
 {
     public class HomeController : Controller
     {
+
+
+
+
+
+
         // GET: Home
         public ActionResult Index()
         {
@@ -26,13 +35,22 @@ namespace JS_Identity.Controllers
         [HttpPost]
         public ActionResult LoginOrCreate(string loginbutton)
         {
-            if (loginbutton == "Log In") { return PartialView("PV_Login"); }
-            else if (loginbutton == "Create New") { return PartialView("PV_CreateAccount"); }
+            if (loginbutton == "Existing User") { return PartialView("PV_Login"); }
+            else if (loginbutton == "New User") { return PartialView("PV_CreateAccount"); }
             else { return View("UserControl"); }
 
         }
 
+        public ActionResult Login()
+        {
+            return View();
+        }
 
+
+        public ActionResult CreateAccount()
+        {
+            return View();
+        }
 
 
 
